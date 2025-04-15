4c7cf4d29459d460269711c0fed664d63a5f4db5
Hanbin Lee <55842199+gaetgoler@users.noreply.github.com>
2025-04-15 16:31:20 +1000

    delete unsued method, fix css issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header"
 import Sidebar from "./components/Sidebar/Sidebar"
